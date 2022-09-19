be3ec8eb6788b967a26ce662f0e2a6ac20196383
Daniel Graham <nexus76@outlook.com>
2022-09-19 02:32:44 -0700

    Scaffolding more

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,72 @@
 import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
+import { createSignal, For } from 'solid-js';
 import styles from './App.module.css';
 
+interface IBitnessValueObject {
+	bitnessValue: BitnessEnum,
+	bitnessDisplay: string
+}
+declare enum BitnessEnum {
+	Bitness8 = 0,
+	Bitness16 = 1,
+	Bitness32 = 2,
+	Bitness64 = 3
+}
+
 const App: Component = () => {
+  const [currentBitness, setCurrentBitness] = createSignal(BitnessEnum.Bitness8);
+	const [bitnessValues, setBitnessValues] = createSignal(new Array<IBitnessValueObject>);
+  
+  const getMaxBitPositions = (bitness: BitnessEnum): Number => {
+    switch (bitness) {
+      case BitnessEnum.Bitness8:
+        return 8;
+      case BitnessEnum.Bitness16:
+        return 16;
+      case BitnessEnum.Bitness32:
+        return 32;
+      case BitnessEnum.Bitness64:
+        return 64;
+      default:
+        return 8;
+    }
+  };
+  const hydrateBitnessValues = (): void => {
+    const bvs = new Array<IBitnessValueObject>();
+    bvs.push({
+      bitnessValue: BitnessEnum.Bitness8,
+      bitnessDisplay: "8 bits"
+    });
+    bvs.push({
+      bitnessValue: BitnessEnum.Bitness16,
+      bitnessDisplay: "16 bits"
+    });
+    bvs.push({
+      bitnessValue: BitnessEnum.Bitness32,
+      bitnessDisplay: "32 bits"
+    });
+    setBitnessValues(bvs);
+    // saving 64 bits for later
+    // this.bitnessValues.push({
+    //   bitnessValue: BitnessEnum.Bitness64,
+    //   bitnessDisplay: "64 bits"
+    // });        
+  }
+  const bitnessChange = (e): void => {
+    console.log(e.currentTarget.value);
+  };
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. Yeah2!
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <div>
+        <span class='bit-flip-bitness'>
+          <label><b>Bitness: </b></label>
+          <select name='bitness' id='bitness' value={currentBitness()} onchange={bitnessChange}>
+            <For each={bitnessValues()} fallback={<div>Loading...</div>}>
+              {(item) => <option value={item.bitnessValue}>{item.bitnessDisplay}</option>}
+            </For>
+          </select>
+        </span>
+      </div>
     </div>
   );
 };
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,2 +1,17 @@
 declare module '*.css';
 declare module '*.svg';
+
+interface IBitValueObject {
+	bitPos: number,
+	bitValue: boolean
+}
+interface IBitnessValueObject {
+	bitnessValue: BitnessEnum,
+	bitnessDisplay: string
+}
+declare enum BitnessEnum {
+	Bitness8 = 0,
+	Bitness16 = 1,
+	Bitness32 = 2,
+	Bitness64 = 3
+}
